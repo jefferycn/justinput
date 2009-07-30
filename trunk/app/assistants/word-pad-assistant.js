7e110c8eed34f76ae9bc57359d1efaa38c6e0851
youjf.com <youjf.com@d4f4b884-7b89-11de-8cd9-f99136019484>
2009-07-30 15:01:58 +0000

    sqlite research

--- trunk/app/assistants/word-pad-assistant.js
+++ trunk/app/assistants/word-pad-assistant.js
@@ -93,6 +93,28 @@ WordPadAssistant.prototype.toggleChange = function() {
 }
 
 WordPadAssistant.prototype.activate = function(event) {
+	try {
+		this.db = openDatabase('JustInput', '', 'JustInput Data Store', 65536);
+		try {
+			// get database config
+			var string = 'select * from config';
+			this.db.transaction(
+        		(function (transaction) {
+            		transaction.executeSql(string, [], this.queryConfigHandler.bind(this), this.createConfigHandler.bind(this));
+        		}).bind(this)
+    		);
+		}catch(e) {
+			var string = 'CREATE TABLE config (id TEXT NOT NULL DEFAULT "", name TEXT NOT NULL DEFAULT ""); GO;';
+			this.db.transaction(
+	        	(function (transaction) {
+					transaction.executeSql('DROP TABLE IF EXISTS config; GO;', []);
+	            	transaction.executeSql(string, [], this.createTableDataHandler.bind(this), this.errorHandler.bind(this));
+	        	}).bind(this)
+	    	);
+		}
+	}catch(e) {
+		$('debug').update(e);
+	}
 }
 
 
@@ -100,4 +122,36 @@ WordPadAssistant.prototype.deactivate = function(event) {
 }
 
 WordPadAssistant.prototype.cleanup = function(event) {
+}
+
+WordPadAssistant.prototype.errorHandler = function(transaction, error) { 
+    console.log('Error was '+error.message+' (Code '+error.code+')'); 
+    return true;
+}
+
+WordPadAssistant.prototype.queryConfigHandler = function(transaction, results) { 
+    var string = "";
+	try {
+		if(results.rows.length > 0) {
+			for (var i = 0; i < results.rows.length; i++) {
+				var row = results.rows.item(i);
+				var name;
+				//string = "";
+				for (name in row) {
+					if (typeof row[name] !== 'function') {
+						string = string + name + ': ' + row[name] + " | ";
+					}
+				}
+			}
+			$('debug').update(string);
+		}else {
+			
+		}
+		//update the list widget
+		//this.resultList.clear();
+		//Object.extend(this.resultList,list);
+		//this.controller.modelChanged(this.listModel, this);
+	}catch (e) {
+		$('result').update(e);	
+	} 
 }
