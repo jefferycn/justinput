4f3d87400a57a61ddba9a672c403f9bab5ac02c4
youjf.com <youjf.com@d4f4b884-7b89-11de-8cd9-f99136019484>
2010-01-03 09:47:48 +0000

    standalone version 1.0.5

---
+++ branches/standalone/trunk/app/assistants/help-assistant.js
@@ -0,0 +1,12 @@
+function HelpAssistant() {
+
+}
+
+HelpAssistant.prototype.setup =  function() {                
+};
+
+HelpAssistant.prototype.activate =  function() {
+};
+
+HelpAssistant.prototype.cleanup =  function() {
+};
