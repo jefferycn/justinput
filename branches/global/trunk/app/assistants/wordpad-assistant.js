c5c9f7ebb56862d7e6f6834cad55c293c9cfec07
youjf.com <youjf.com@d4f4b884-7b89-11de-8cd9-f99136019484>
2010-01-13 14:07:58 +0000

    justinput application modified

--- branches/global/trunk/app/assistants/wordpad-assistant.js
+++ branches/global/trunk/app/assistants/wordpad-assistant.js
@@ -11,7 +11,6 @@ WordpadAssistant.prototype.setup = function() {
 			}, this.model = {});
 
 	// document.observe('keypress', this.turnOn.bind(this));
-	// this.db = new Database("ext:JustInput", "1", this.loadDB.bind(this));
 
 	// var target = Mojo.View.getFocusedElement(this.controller.sceneElement);
 	// Mojo.Log.info("initialize ======> " +
@@ -28,60 +27,35 @@ WordpadAssistant.prototype.setup = function() {
 				omitDefaultItems : true
 			}, this.appMenuModel);
 
-	this.cmdMenuModel = {
-		items : [{
-					toggleCmd : 'imeOn',
-					items : [{
-								label : $L('中文'),
-								command : 'imeOn'
-							}, {
-								label : $L('英文'),
-								command : 'imeOff'
-							}]
-				}, {
-					items : [{
-								label : $L('短信'),
-								command : 'imeMessaging'
-							}, {
-								label : $L('清空'),
-								command : 'imeClean'
-							}]
-				}]
+	this.buttonModel = {
+		buttonLabel : "清空",
+		buttonClass : 'secondary'
 	};
-	this.controller.setupWidget(Mojo.Menu.commandMenu, undefined,
-			this.cmdMenuModel);
+	this.controller.setupWidget('empty', {
+				type : 'default'
+			}, this.buttonModel);
+	$('empty').observe('click', this.emptyText.bind(this), true);
+	$('text').observe(Mojo.Event.propertyChange, this.textOnPropertyChange.bind(this), true);
+}
+
+WordpadAssistant.prototype.textOnPropertyChange = function(e) {
+	var text = $('text');
+	var result = text.mojo.getValue();
+	if (!result) {
+		return false;
+	}
+	var pos = text.mojo.getCursorPosition();
+	text.mojo.setCursorPosition(0, result.length);
+	document.execCommand('copy');
+	text.mojo.setCursorPosition(pos.selectionStart, pos.selectionStart);
+}
+
+WordpadAssistant.prototype.emptyText = function(event) {
+	$('text').mojo.setValue('');
 }
 
 WordpadAssistant.prototype.handleCommand = function(event) {
 	if (event.type == Mojo.Event.command) {
-		if (event.command == 'imeOn' || event.command == 'imeOff') {
-			this.toggleChange();
-		}
-		if (event.command == 'imeCopy') {
-			var text = this.controller.get('text');
-			text.focus();
-			text.mojo.setCursorPosition(0, text.mojo.getValue().length);
-			this.controller.doExecCommand(event, 'copy');
-		}
-		if (event.command == 'imePaste') {
-			this.controller.get('text').focus();
-			PalmSystem.paste();
-		}
-		if (event.command == 'imeClean') {
-			this.controller.get('text').mojo.setValue('');
-		}
-		if (event.command == 'imeMessaging') {
-			this.controller.serviceRequest(
-					'palm://com.palm.applicationManager', {
-						method : 'launch',
-						parameters : {
-							id : "com.palm.app.messaging",
-							params : {
-								messageText : $('text').mojo.getValue()
-							}
-						}
-					});
-		}
 		if (event.command == 'help') {
 			Mojo.Controller.stageController.pushScene({
 						'name' : 'help',
@@ -91,53 +65,8 @@ WordpadAssistant.prototype.handleCommand = function(event) {
 	}
 }
 
-WordpadAssistant.prototype.toggleChange = function() {
-	this.ime.toggleIme();
-}
-
 WordpadAssistant.prototype.deactivate = function(event) {
 }
 
 WordpadAssistant.prototype.cleanup = function(event) {
-}
-
-var timePress = 0;
-var ime = undefined;
-var loaded = false;
-document.onkeypress = function(event) {
-	if (event.keyCode == 231 || event.keyCode == 51) {
-		if (timePress == 0) {
-			timePress++;
-			setTimeout(cleanTimer, 900);
-		} else {
-			if (loaded == false) {
-				loadJS('PinyingSource');
-				loadJS('PreFixMap');
-				loadJS('IME');
-				loaded = true;
-			}
-			if (typeof(ime) == "undefined") {
-				setTimeout(startIME, 200);
-			} else {
-				ime.uninstall();
-				ime = undefined;
-			}
-		}
-		event.returnValue = false;
-	}
-}
-
-function loadJS(name) {
-	var element = document.createElement('script');
-	element.setAttribute('src', '/usr/palm/frameworks/mojo/justinput/' + name + '.js');
-	element.setAttribute('type', 'text/javascript');
-	document.body.appendChild(element);
-}
-
-function cleanTimer() {
-	timePress = 0;
-}
-
-function startIME() {
-	ime = new IME();
 }
