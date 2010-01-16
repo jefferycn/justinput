21454e9c566a7cbfff81a972f404c6932cf682d0
youjf.com <youjf.com@d4f4b884-7b89-11de-8cd9-f99136019484>
2010-01-16 02:58:25 +0000

    files cleanup

--- trunk/app/assistants/wordpad-assistant.js
+++ trunk/app/assistants/wordpad-assistant.js
@@ -10,9 +10,11 @@ WordpadAssistant.prototype.setup = function() {
 				changeOnKeyPress : true
 			}, this.model = {});
 
-	this.timePress = 0;
-	//document.observe('keypress', this.turnOn.bind(this));
-	this.db = new Database("ext:JustInput", "1", this.loadDB.bind(this));
+	// document.observe('keypress', this.turnOn.bind(this));
+
+	// var target = Mojo.View.getFocusedElement(this.controller.sceneElement);
+	// Mojo.Log.info("initialize ======> " +
+	// Mojo.Log.propertiesAsString(this.text));
 
 	this.appMenuModel = {
 		visible : true,
@@ -25,88 +27,35 @@ WordpadAssistant.prototype.setup = function() {
 				omitDefaultItems : true
 			}, this.appMenuModel);
 
-	this.cmdMenuModel = {
-		items : [{
-					toggleCmd : 'imeOn',
-					items : [{
-								label : $L('中文'),
-								command : 'imeOn'
-							}, {
-								label : $L('英文'),
-								command : 'imeOff'
-							}]
-				}, {
-					items : [{
-								label : $L('短信'),
-								command : 'imeMessaging'
-							}, {
-								label : $L('清空'),
-								command : 'imeClean'
-							}]
-				}]
+	this.buttonModel = {
+		buttonLabel : "清空",
+		buttonClass : 'secondary'
 	};
-	this.controller.setupWidget(Mojo.Menu.commandMenu, undefined,
-			this.cmdMenuModel);
+	this.controller.setupWidget('empty', {
+				type : 'default'
+			}, this.buttonModel);
+	$('empty').observe('click', this.emptyText.bind(this), true);
+	$('text').observe(Mojo.Event.propertyChange, this.textOnPropertyChange.bind(this), true);
 }
 
-WordpadAssistant.prototype.turnOn = function(event) {
-	if (event.keyCode == 231 || event.keyCode == 51) {
-		if (this.timePress == 0) {
-			this.timePress++;
-			setTimeout('this.timePress = 0', 1250);
-		} else {
-			// Mojo.Log.info("text => " +
-			// Mojo.Log.propertiesAsString($('text')));
-			// Mojo.Log.info("turnOn => " +
-			// Mojo.Log.propertiesAsString(this.controller));
-			if (typeof(this.ime) == "undefined") {
-				this.db = new Database("ext:JustInput", "1", this.loadDB.bind(this));
-			}
-		}
-		event.returnValue = false;
+WordpadAssistant.prototype.textOnPropertyChange = function(e) {
+	var text = $('text');
+	var result = text.mojo.getValue();
+	if (!result) {
+		return false;
 	}
+	var pos = text.mojo.getCursorPosition();
+	text.mojo.setCursorPosition(0, result.length);
+	document.execCommand('copy');
+	text.mojo.setCursorPosition(pos.selectionStart, pos.selectionStart);
 }
 
-WordpadAssistant.prototype.loadDB = function(isReady) {
-	// Mojo.Log.info("loadDB; ready = " + isReady);
-	if (isReady == false) {
-		Mojo.Controller.errorDialog("can not open database");
-		return;
-	}
-	var target = Mojo.View.getFocusedElement(this.controller.sceneElement);
-	this.ime = new IME(target, this.db);
+WordpadAssistant.prototype.emptyText = function(event) {
+	$('text').mojo.setValue('');
 }
 
 WordpadAssistant.prototype.handleCommand = function(event) {
 	if (event.type == Mojo.Event.command) {
-		if (event.command == 'imeOn' || event.command == 'imeOff') {
-			this.toggleChange();
-		}
-		if (event.command == 'imeCopy') {
-			var text = this.controller.get('text');
-			text.focus();
-			text.mojo.setCursorPosition(0, text.mojo.getValue().length);
-			this.controller.doExecCommand(event, 'copy');
-		}
-		if (event.command == 'imePaste') {
-			this.controller.get('text').focus();
-			PalmSystem.paste();
-		}
-		if (event.command == 'imeClean') {
-			this.controller.get('text').mojo.setValue('');
-		}
-		if (event.command == 'imeMessaging') {
-			this.controller.serviceRequest(
-					'palm://com.palm.applicationManager', {
-						method : 'launch',
-						parameters : {
-							id : "com.palm.app.messaging",
-							params : {
-								messageText : $('text').mojo.getValue()
-							}
-						}
-					});
-		}
 		if (event.command == 'help') {
 			Mojo.Controller.stageController.pushScene({
 						'name' : 'help',
@@ -114,10 +63,6 @@ WordpadAssistant.prototype.handleCommand = function(event) {
 					});
 		}
 	}
-}
-
-WordpadAssistant.prototype.toggleChange = function() {
-	this.ime.toggleIme();
 }
 
 WordpadAssistant.prototype.deactivate = function(event) {
