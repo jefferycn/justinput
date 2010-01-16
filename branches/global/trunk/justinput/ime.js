f837f29e065b2a753880a3eab5a3ee661c3bc8df
youjf.com <youjf.com@d4f4b884-7b89-11de-8cd9-f99136019484>
2010-01-16 09:46:04 +0000

    remove bindMe function

--- branches/global/trunk/justinput/ime.js
+++ branches/global/trunk/justinput/ime.js
@@ -110,42 +110,6 @@ IME.prototype = {
 			c.select()
 		}
 	},
-	bindMe :  function(target) {
-		this.active = true;
-		if (handlerBox.fxTextOnKeyDown) {
-			this.stopObservingKeyDown = handlerBox.fxTextOnKeyDown;
-		}
-		if (handlerBox.fxTextOnKeyPress) {
-			this.stopObservingKeyPress = handlerBox.fxTextOnKeyPress;
-		}
-		handlerBox = {};
-		handlerBox.fxTextOnKeyDown = this.textOnKeyDown.bind(this);
-		handlerBox.fxTextOnKeyPress = this.textOnKeyPress.bind(this);
-		if (this.stopObservingKeyDown) {
-			target.stopObserving('keydown', this.stopObservingKeyDown, true);
-		}
-		if (this.stopObservingKeyPress) {
-			target.stopObserving('keypress', this.stopObservingKeyPress, true);
-		}
-		target.observe('keydown', handlerBox.fxTextOnKeyDown, true);
-		target.observe('keypress', handlerBox.fxTextOnKeyPress, true);
-	},
-	unbindMe : function(target) {
-		this.active = false;
-		if (handlerBox.fxTextOnKeyDown) {
-			this.stopObservingKeyDown = handlerBox.fxTextOnKeyDown;
-		}
-		if (handlerBox.fxTextOnKeyPress) {
-			this.stopObservingKeyPress = handlerBox.fxTextOnKeyPress;
-		}
-		handlerBox = {};
-		if (this.stopObservingKeyDown) {
-			target.stopObserving('keydown', this.stopObservingKeyDown, true);
-		}
-		if (this.stopObservingKeyPress) {
-			target.stopObserving('keypress', this.stopObservingKeyPress, true);
-		}
-	},
 	toggleIme : function() {
 		if (this.active == true) {
 			this.active = false;
