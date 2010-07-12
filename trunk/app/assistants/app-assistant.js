092adf1c42a55b0f5867d0bf886db6aaaf23d10c
youjf.com <youjf.com@d4f4b884-7b89-11de-8cd9-f99136019484>
2010-07-12 10:43:39 +0000

    prepare for new version release

--- trunk/app/assistants/app-assistant.js
+++ trunk/app/assistants/app-assistant.js
@@ -5,18 +5,24 @@ AppAssistant.prototype.handleLaunch = function(params) {
 	if (params && params.action) {
                 switch (params.action) {
                         default:
-                                this.launchWordPad();
+                                this.launchDesktop();
                                 break;
                 }
         } else {
-                this.launchWordPad();
+                this.launchDesktop();
         }
 }
 
-AppAssistant.prototype.launchWordPad = function() {
-	var f = function(stageController) {
-    	stageController.pushScene('wordpad');
-    }.bind(this);
-    this.controller.createStageWithCallback({name: 'wordpad', lightweight: true}, f);
-	//this.controller.pushScene('wordpad');
+AppAssistant.prototype.launchDesktop = function() {
+	var stageName = "justin-desktop";
+	var dashStage = Mojo.Controller.appController.getStageProxy(stageName);
+
+    if (dashStage) {
+        dashStage.activate();
+    } else {
+		var f = function(stageController) {
+    		stageController.pushScene('desktop');
+    	}.bind(this);
+    	this.controller.createStageWithCallback({name: stageName, lightweight: true}, f);
+    }
 }
---
+++ trunk/app/assistants/donate-assistant.js
@@ -0,0 +1,12 @@
+function DonateAssistant() {
+
+}
+
+DonateAssistant.prototype.setup =  function() {                
+};
+
+DonateAssistant.prototype.activate =  function() {
+};
+
+DonateAssistant.prototype.cleanup =  function() {
+};
